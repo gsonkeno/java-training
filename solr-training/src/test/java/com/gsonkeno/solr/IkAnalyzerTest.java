5d2b862816b3570a0b10104e2d4cd5a6db1a1dd8
高嵩 <gaosong@vvic.com>
2018-07-12 12:51:34 +0800

    add solr training

--- solr-training/src/test/java/com/gsonkeno/solr/IkAnalyzerTest.java
+++ solr-training/src/test/java/com/gsonkeno/solr/IkAnalyzerTest.java
@@ -1,39 +1,14 @@
 package com.gsonkeno.solr;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.junit.Test;
-import org.wltea.analyzer.lucene.IKAnalyzer;
-
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class IkAnalyzerTest {
     @Test
     public void testAnalyzer() {
-        List<String> list = new ArrayList<String>();
-        try {
-            //创建分词对象
-            Analyzer anal = new IKAnalyzer(true);
-            StringReader reader = new StringReader("搜款网饺子");
-
-            //分词
-            TokenStream ts = anal.tokenStream("", reader);
-            CharTermAttribute term = ts.getAttribute(CharTermAttribute.class);
-            ts.reset();
-
-            //遍历分词数据
-            while (ts.incrementToken()) {
-                list.add(term.toString());
-            }
-           reader.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(list);
+        //ik分词器会过滤字符串中最前面特殊字符
+        System.out.println(IkAnalyzerUtils.analyzer("-716-"));
+        System.out.println(IkAnalyzerUtils.analyzer("+716+"));
+        System.out.println(IkAnalyzerUtils.analyzer("$716$"));
+        System.out.println(IkAnalyzerUtils.analyzer("^716^"));
     }
 }
