de904e2968696ed5f09d4bb5f7278d315a80b2d7
gaosong <805940564@qq.com>
2018-08-26 00:45:40 +0800

    add collections training

---
+++ collection-training/src/main/java/com/gsonkeno/collectiontraining/CollectionTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.gsonkeno.collectiontraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionTrainingApplication.class, args);
+    }
+}
