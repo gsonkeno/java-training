895fb4fcfc0797f76ae41d9d024c015e0d54751c
gaosong <805940564@qq.com>
2019-04-05 21:10:29 +0800

    del

--- jdk-training/src/main/java/com/gsonkeno/jdk/training/concurrent/StopThreadUse.java
+++
@@ -1,9 +0,0 @@
-package com.gsonkeno.jdk.training.concurrent;
-
-/**
- * 终止线程的方式
- * 使用线程的中断状态标识位 或者 线程内部维护一个boolean变量来终止线程
- * Created by gaosong on 2017-08-31
- */
-public class StopThreadUse {
-}
